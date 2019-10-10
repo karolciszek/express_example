9cac64ee5e42b6154e871b7a15fb66c1b02e1417
Karol Ciszek <karol.j.ciszek@gmail.com>
2019-10-10 15:10:57 +0100

    Install express

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 
-const server = http.createServer(
-    (req, res) => {
-        console.log(req)
-        res.setHeader('Content-Type', 'text/html')
-        res.write('<h1>Hewwo uwu Rawr x3</h1>')
-        res.write('<h3>*nuzzles* how are you senpaii</h3>')
-        res.end()
-    }
-)
+app.get('/', (req, res) => {
+    console.log(req)
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<h1>Hewwo world uwu</h1>')
+    res.write('<h3>senpai will anata-wa notice my reqwest? ^_^</h3>')
+    res.send()
+})
 
-server.listen(3000)
+app.listen(3000)
